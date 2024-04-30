ad8833cfe23ac5b92426293f5031ec0613133e75
sdeokar13599 <69460973+sdeokar13599@users.noreply.github.com>
2024-04-30 10:19:47 +0530

    Create Programm4.py

---
+++ Programm4.py
@@ -0,0 +1,13 @@
+## Swap two numbers
+a=input("Enter the value of a")
+b=input("Enter the valude of b")
+print("Before Swaping")
+print("The value of a is-->"+a)
+print("The value of b is-->"+b)
+
+temp = a
+a = b 
+b  = temp 
+print("After Swaping")
+print("The value of a is-->"+a)
+print("The value of b is-->"+b)
