f6b5db44b1da61e201ecc0ba495f1ef1fa75903b
Shubham Deokar <sdeokar13599@gmail.com>
2025-08-06 20:29:55 +0530

    string code added

---
+++ input_string.py
@@ -0,0 +1,21 @@
+# name=input("Enter you name")
+# print("Hello",name)
+
+# code uppercase or lowercase the string the string--.
+
+name='sHubham'
+choice=input("Enter what u want to ")
+
+def c_upper(name):
+    print(name.upper())
+
+def c_lower(name):
+    print(name.lower())   
+
+if choice=='u':
+    c_upper(name)
+else:
+    c_lower(name)
+
+
+
